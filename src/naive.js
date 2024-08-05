a2438172fb8db189fdf9439530c049fefcd2ee80
Eric Moore <EricGittaMoore@duck.com>
2024-08-05 19:35:47 +0800

    fix: es

--- src/naive.js
+++ src/naive.js
@@ -2,48 +2,51 @@ const isFunction = obj => typeof obj === 'function'
 const isObject = obj => !!(obj && typeof obj === 'object')
 const isThenable = obj => (isFunction(obj) || isObject(obj)) && 'then' in obj
 const isPromise = promise => promise instanceof Promise
+const nextTick = queueMicrotask || setTimeout
 
 const PENDING = 'pending'
 const FULFILLED = 'fulfilled'
 const REJECTED = 'rejected'
 
-function Promise(f) {
-  this.result = null
-  this.state = PENDING
-  this.callbacks = []
+class Promise {
+  constructor(fn) {
+    this.result = null
+    this.state = PENDING
+    this.callbacks = []
 
-  let onFulfilled = value => transition(this, FULFILLED, value)
-  let onRejected = reason => transition(this, REJECTED, reason)
+    let onFulfilled = value => transition(this, FULFILLED, value)
+    let onRejected = reason => transition(this, REJECTED, reason)
 
-  let ignore = false
-  let resolve = value => {
-    if (ignore) return
-    ignore = true
-    resolvePromise(this, value, onFulfilled, onRejected)
-  }
-  let reject = reason => {
-    if (ignore) return
-    ignore = true
-    onRejected(reason)
-  }
-
-  try {
-    f(resolve, reject)
-  } catch (error) {
-    reject(error)
-  }
-}
-
-Promise.prototype.then = function(onFulfilled, onRejected) {
-  return new Promise((resolve, reject) => {
-    let callback = { onFulfilled, onRejected, resolve, reject }
-
-    if (this.state === PENDING) {
-      this.callbacks.push(callback)
-    } else {
-      setTimeout(() => handleCallback(callback, this.state, this.result), 0)
+    let ignore = false
+    let resolve = value => {
+      if (ignore) return
+      ignore = true
+      resolvePromise(this, value, onFulfilled, onRejected)
     }
-  })
+    let reject = reason => {
+      if (ignore) return
+      ignore = true
+      onRejected(reason)
+    }
+
+    try {
+      fn(resolve, reject)
+    } catch (error) {
+      reject(error)
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    return new Promise((resolve, reject) => {
+      let callback = { onFulfilled, onRejected, resolve, reject }
+
+      if (this.state === PENDING) {
+        this.callbacks.push(callback)
+      } else {
+        nextTick(() => handleCallback(callback, this.state, this.result), 0)
+      }
+    })
+  }
 }
 
 const handleCallback = (callback, state, result) => {
@@ -67,7 +70,7 @@ const transition = (promise, state, result) => {
   if (promise.state !== PENDING) return
   promise.state = state
   promise.result = result
-  setTimeout(() => handleCallbacks(promise.callbacks, state, result), 0)
+  nextTick(() => handleCallbacks(promise.callbacks, state, result), 0)
 }
 
 const resolvePromise = (promise, result, resolve, reject) => {
