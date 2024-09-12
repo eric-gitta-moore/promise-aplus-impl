2cbf07c45e1555afeea6434c5eafb4f832508bb4
Eric Moore <EricGittaMoore@duck.com>
2024-09-12 10:55:54 +0800

    update

--- await-down-grade.js
+++ await-down-grade.js
@@ -2,50 +2,91 @@
 
 /*
 async function main(){
-  await 1;
+  console.log(await 1);
 }
 main()
 */
 
 
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
-    try {
-      var info = gen[key](arg);
-      var value = info.value;
-    } catch (error) {
-      reject(error);
-      return;
-    }
-    if (info.done) {
-      resolve(value);
-    } else {
-      Promise.resolve(value).then(_next, _throw);
-    }
+  try {
+    var info = gen[key](arg);
+    var value = info.value;
+  } catch (error) {
+    reject(error);
+    return;
   }
-  function _asyncToGenerator(fn) {
-    return function () {
-      var self = this,
-        args = arguments;
-      return new Promise(function (resolve, reject) {
-        var gen = fn.apply(self, args);
-        function _next(value) {
-          asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
-        }
-        function _throw(err) {
-          asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
-        }
-        _next(undefined);
-      });
-    };
+  if (info.done) {
+    resolve(value);
+  } else {
+    Promise.resolve(value).then(_next, _throw);
   }
-  function main() {
-    return _main.apply(this, arguments);
-  }
-  function _main() {
-    _main = _asyncToGenerator(function* () {
-      yield 1;
+}
+function _asyncToGenerator(fn) {
+  return function () {
+    var self = this,
+      args = arguments;
+    return new Promise(function (resolve, reject) {
+      var gen = fn.apply(self, args);
+      function _next(value) {
+        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
+      }
+      function _throw(err) {
+        asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
+      }
+      _next(undefined);
     });
-    return _main.apply(this, arguments);
+  };
+}
+function main() {
+  return _main.apply(this, arguments);
+}
+function _main() {
+  _main = _asyncToGenerator(function* () {
+    console.log(yield 1);
+  });
+  return _main.apply(this, arguments);
+}
+main();
+
+
+
+// ---
+(async function name() {
+  console.log(await 1)
+})()
+
+
+  (function name() {
+    function* _name() {
+      console.log(yield 1)
+    }
+    return _asyncToGenerator(_name).apply(this, arguments)
+  })()
+
+function asyncGeneratorStep({ gen, resolve, reject, _next, _throw, key, arg }) {
+  try {
+    var info = gen[key](arg)
+    var val = info.value
+  } catch (e) {
+    reject(e)
+    return
   }
-  main();
-  
+  if (info.done) resolve(val)
+  else Promise.resolve(val).then(_next, _throw)
+}
+
+function _asyncToGenerator(fn) {
+  return function () {
+    const { promise, resolve, reject } = Promise.withResolvers()
+    let gen = fn.apply(this, arguments)
+    function _next(arg) {
+      asyncGeneratorStep({ gen, resolve, reject, _next, _throw, key: 'next', arg })
+    }
+    function _throw(arg) {
+      asyncGeneratorStep({ gen, resolve, reject, _next, _throw, key: 'throw', arg })
+    }
+    _next(null)
+    return promise
+  }
+}
